3cbb7fd50d58fcc5fe4be3e0c5446a9f0d823fbe
Qmppu842 <omppu842@gmail.com>
2021-05-14 16:28:34 +0300

    Added VisUI load function.

--- core/src/main/java/fi/qmppu842/fisutankki/Main.kt
+++ core/src/main/java/fi/qmppu842/fisutankki/Main.kt
@@ -1,11 +1,23 @@
 package fi.qmppu842.fisutankki
 
-import com.badlogic.gdx.Game
-import fi.qmppu842.fisutankki.FirstScreen
+import com.badlogic.gdx.Gdx
+import com.kotcrab.vis.ui.VisUI
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.log.debug
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
-class Main : Game() {
+class Main : KtxGame<KtxScreen>() {
     override fun create() {
-        setScreen(FirstScreen())
+
+        addScreen(FirstScreen())
+
+        setScreen<FirstScreen>()
+
+        try {
+            VisUI.load()
+        } catch (e: Exception) {
+            debug("[Ui Skin]") { "Vis UI was probably loaded somewhere else." }
+
+        }
     }
 }
