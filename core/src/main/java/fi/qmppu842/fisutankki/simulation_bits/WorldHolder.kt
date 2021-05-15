7b0df022f6893bba27654276ee9aa53330075c9b
Qmppu842 <omppu842@gmail.com>
2021-05-16 01:09:23 +0300

    Added basics for Fish life supports.

--- core/src/main/java/fi/qmppu842/fisutankki/simulation_bits/WorldHolder.kt
+++ core/src/main/java/fi/qmppu842/fisutankki/simulation_bits/WorldHolder.kt
@@ -1,11 +1,14 @@
 package fi.qmppu842.fisutankki.simulation_bits
 
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
+import ktx.box2d.body
+import ktx.box2d.circle
 import ktx.box2d.createWorld
 
 class WorldHolder {
-    val fishList = ArrayList<Fish>()
-    val gravity = Vector2.Zero
+    private val fishList = ArrayList<Fish>()
+    private val gravity = Vector2.Zero
     val world = createWorld(gravity = gravity, true)
 
     companion object WorldObject {
@@ -18,5 +21,16 @@ class WorldHolder {
 
     fun update(dt: Float) {
         world.step(dt, 6, 2)
+    }
+
+    fun addFishToWorld() {
+        val radius = 100f
+        val body: Body = world.body{
+            circle(radius = radius){
+                restitution =0.5f
+            }
+        }
+        val fisu = Fish(body)
+        fishList.add(fisu)
     }
 }
