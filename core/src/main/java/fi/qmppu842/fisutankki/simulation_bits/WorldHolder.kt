4aff3e22d76b6babd9564947a64a620513355987
Qmppu842 <omppu842@gmail.com>
2021-05-19 20:36:05 +0300

    Added method to add first wall.

--- core/src/main/java/fi/qmppu842/fisutankki/simulation_bits/WorldHolder.kt
+++ core/src/main/java/fi/qmppu842/fisutankki/simulation_bits/WorldHolder.kt
@@ -2,7 +2,10 @@ package fi.qmppu842.fisutankki.simulation_bits
 
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.BodyDef
 import fi.qmppu842.fisutankki.GlobalVariables
+import ktx.box2d.body
+import ktx.box2d.box
 import ktx.box2d.createWorld
 
 class WorldHolder {
@@ -15,6 +18,7 @@ class WorldHolder {
 
     init {
         world.setContactListener(MyContactListener())
+//        addWalls()
     }
 
     companion object WorldObject {
@@ -28,7 +32,7 @@ class WorldHolder {
     }
 
     fun update(dt: Float) {
-        var dt2 = dt* gVars.dtMultiplier
+        var dt2 = dt * gVars.dtMultiplier
         world.step(dt2, 6, 2)
         for (fish: Fish in fishList) {
             fish.update(dt2)
@@ -36,7 +40,7 @@ class WorldHolder {
     }
 
     fun addSchoolOfFishToWorld() {
-        for (i in 1..13) {
+        for (i in 1..20) {
             var fishe = Fish.addRandomFishToWorld(world)
             fishList.add(fishe)
             fishNameMap[fishe.name] = fishe
@@ -45,5 +49,25 @@ class WorldHolder {
 
     fun getFish(name: String): Fish? {
         return fishNameMap[name]
+    }
+
+    private fun addWalls() {
+        var wall1 = world.body(type = BodyDef.BodyType.StaticBody) {
+            position.set(0f, -gVars.outsideBorderSize / 2)
+            box(width = gVars.sWidth, height = gVars.outsideBorderSize) {
+                filter.categoryBits = Fish.fishFilter.toShort()
+                filter.maskBits = (Fish.fishFilter or Fish.repulseFilter).toShort()
+            }
+
+        }
+//        var wall2 = world.body(type = BodyDef.BodyType.StaticBody) {
+//            position.set(0f, gVars.outsideBorderSize / 2)
+//            box(width = gVars.sWidth, height = gVars.outsideBorderSize) {
+//                filter.categoryBits = Fish.fishFilter.toShort()
+//                filter.maskBits = (Fish.fishFilter or Fish.repulseFilter).toShort()
+//            }
+//
+//        }
+
     }
 }
